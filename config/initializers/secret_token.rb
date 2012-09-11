4ad1a04f29ee11ec39d30cb916f5f0e2955b5e28
Lumren Randir <lumrandir@gmail.com>
2012-09-11 12:46:15 +0600

    rails app sceleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pjos::Application.config.secret_token = 'b326c41917b4ef880fbedc4faa251717fe2a1492c26cc2ce1977592dbbe182aae35bf8daf8ad5c516d53706c19cea66b8d050ef0289cc91cf53c3dd3b8c5c0cb'
